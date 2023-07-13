891630d11a8979ce81accdc154202406fcd978e8
chirag garg <chirag@myrm.io>
2023-07-13 19:51:26 +0530

    login_api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,30 @@
 class SessionsController < ApplicationController
-	# before_action :authentication, only: [:login]
 
-	def signup
-		byebug
-		user = User.new(email: params[:email], password: params[:password], role:params[:role])
-		if user.save
-			token = encode_user_data({ user_data: user.id })
-			render json: { token: token }
-		else
-			render json: { message: "invalid credentials" }
-		end
-	end
+  def signup
+    byebug
+    user = User.new(email: params[:email], password: params[:password], role:params[:role])
+    if user.save
+      token = encode_user_data({ user_data: user.id })
+      render json: { token: token }
+    else
+      render json: { message: "invalid credentials" }
+    end
+  end
 
+  def login
+    byebug
+    user = User.find_by(email: params[:email], password: params[:password])
+    if user && user.password == params[:password] && allowed_roles.include?(user.role.to_sym)
+      token = encode_user_data({ user_data: user.id })
+      render json: { token: token }
+    else
+      render json: { message: "invalid credentials" }
+    end
+  end
 
+  private
 
-	def login
-		user = User.find_by(email: params[:email])
-		if user && user.password == params[:password]
-			token = encode_user_data({ user_data: user.id })
-			render json: { token: token }
-		else
-			render json: { message: "invalid credentials" }
-		end
-	end
+  def allowed_roles
+    [:admin, :patient, :doctor]
+  end
 end
